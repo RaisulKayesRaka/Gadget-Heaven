639c52b12c8f7f74317a4a6f0a0e6c7860736ec0
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-11-03 20:33:17 +0600

    Initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
+    </>
+  )
+}
+
+export default App
